6cac1834225d59221ecbf4df0aecd7dbcfe635dd
DanielThedie <dthedie@gmail.com>
2026-06-12 14:19:54 +0100

    Timeseries plot to compare multiple datasets
    * Handles an arbitrary number of sessions datasets
    * Can plot numeric or datetime data

---
+++ R/timeseries_comparison.R
@@ -0,0 +1,87 @@
+timeseries_comparison <- function(sessions_list, variable) {
+
+  data <- data.frame()
+  nights_list <- vector("list", length(sessions_list))
+  is_time <- is_iso8601_datetime(sessions_list[[1]][[variable[[1]]]])
+
+  for (i in seq_along(sessions_list)) {
+    s <- sessions_list[[i]]
+    col <- get_colnames(s)
+
+    if (length(variable) == 1) {
+      var <- variable
+    } else if (length(variable) == length(sessions_list)) {
+      var <- variable[[i]]
+    } else {
+      cli::cli_abort(c(
+        "x" = "{.var variable must contain either one value, or as many as sessions in {.var sessions_list}",
+        "i" = "You provided {len(variable)} values",
+        "i" = "{.var sessions_list} contains {len(sessions_list)} values"
+      ))
+    }
+
+    var_type <- is_iso8601_datetime(s[[var]])
+    if (!is.numeric(class(s[[var]])) && !var_type) {
+      cli::cli_abort(c(
+        "x" = "{.var variable} column must be either numeric or datetime",
+        "i" = "{class(s[[var]]) detected}"
+      ))
+    }
+    if (var_type != is_time) {
+      cli::cli_abort(c(
+        "x" = "{.var variable} data type must be either numeric or datetime for all dataframes",
+        "i" = "Sessions {names(sessions_list)[[i]]} have an inconsistent type ({var_type})"
+      ))
+    }
+
+    s <- s |>
+      dplyr::mutate(
+        night = .data[[col$night]],
+        value = {
+          if (is_time) {
+            parse_time(.data[[var]]) |>
+              shift_times_by_12h() |>
+              time_to_hours()
+          } else {
+            .data[[var]]
+          }
+        },
+        label = names(sessions_list)[[i]]
+      ) |>
+      dplyr::filter(!is.na(.data$value))
+
+    nights_list[[i]] <- s |>
+      dplyr::distinct(.data$night) |>
+      dplyr::pull(.data$night)
+
+    data <- dplyr::bind_rows(data, s)
+  }
+
+  common_nights <- Reduce(intersect, nights_list)
+  data <- data |>
+    dplyr::filter(.data$night %in% common_nights)
+
+  p <- ggplot2::ggplot(data, ggplot2::aes(
+    x = .data$night,
+    y = .data$value,
+    colour = .data$label
+  )) +
+    ggplot2::geom_point(size = 3) +
+    ggplot2::labs(
+      x = NULL,
+      y = variable[1],
+      color = NULL
+    ) +
+    ggplot2::theme_minimal(base_size = 16)
+
+  if (is_time) {
+    p <- p +
+      ggplot2::scale_y_continuous(
+        breaks = seq(0, 24, by = 2),
+        labels = function(x) sprintf("%02d:00", (x + 12) %% 24),
+        expand = ggplot2::expansion(mult = c(0.08, 0.08))
+      )
+  }
+
+  p
+}
